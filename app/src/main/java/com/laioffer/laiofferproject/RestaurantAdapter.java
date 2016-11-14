621ec2152ff888fe7015b5814362e6c083f2948d
paige0426 <paige0426@g.ucla.edu>
2016-11-13 17:05:56 -0800

    Now can rate restaurants!!

--- app/src/main/java/com/laioffer/laiofferproject/RestaurantAdapter.java
+++ app/src/main/java/com/laioffer/laiofferproject/RestaurantAdapter.java
@@ -1,11 +1,13 @@
 package com.laioffer.laiofferproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.LayerDrawable;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +19,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
 
 import java.util.List;
 
@@ -58,7 +62,7 @@ public class RestaurantAdapter extends BaseAdapter {
     }
 
 
-    @Override
+/*    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) context.getSystemService(
@@ -78,13 +82,13 @@ public class RestaurantAdapter extends BaseAdapter {
         final LayerDrawable stars = (LayerDrawable) ratingBar.getProgressDrawable();
         stars.getDrawable(2).setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP);
 
-        /*restaurantThumbnail.setOnClickListener(new View.OnClickListener() {
+        *//*restaurantThumbnail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 stars.getDrawable(2).setColorFilter(Color.BLACK, PorterDuff.Mode.SRC_ATOP);
                 Log.e("Life", "try");
             }
-        });*/
+        });*//*
 
         final ImageView visited = (ImageView) convertView.findViewById(R.id.imageView);
         final Restaurant r = restaurantData.get(position);
@@ -107,7 +111,7 @@ public class RestaurantAdapter extends BaseAdapter {
             }
         });
         return convertView;
-    }
+    }*/
 
     private void setImageView(ImageView visited, boolean val) {
         if (val == true) {
@@ -116,4 +120,85 @@ public class RestaurantAdapter extends BaseAdapter {
             visited.setImageResource(R.drawable.presence_invisible);
         }
     }
+
+    class Holder {
+        TextView restaurantName;
+        TextView restaurantAddress;
+        TextView restaurantType;
+        ImageView restaurantThumbnail;
+        RatingBar ratingBar;
+        ImageView visited;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Holder holder = null;
+        final Restaurant r = restaurantData.get(position);
+        if (convertView == null) {
+            LayoutInflater inflater =
+                    (LayoutInflater) context.getSystemService(
+                            Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.activity_restaurant_list_item,
+                    parent, false);
+            holder = new Holder();
+            holder.restaurantName = (TextView) convertView.findViewById(
+                    R.id.restaurant_name);
+            holder.restaurantAddress = (TextView) convertView.findViewById(
+                    R.id.restaurant_address);
+            holder.restaurantType = (TextView) convertView.findViewById(
+                    R.id.restaurant_type);
+
+
+            holder.restaurantThumbnail = (ImageView) convertView.findViewById(
+                    R.id.restaurant_thumbnail);
+            holder.ratingBar = (RatingBar) convertView.findViewById(R.id.restaurant_rating);
+            holder.visited = (ImageView) convertView.findViewById(R.id.imageView);
+            setImageView(holder.visited, r.isVisited());
+            final String businessId = r.getBusinessId();
+            final Holder finalHolder = holder;
+            holder.visited.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listFragment.addDeleteJson(businessId, r.isVisited());
+                    //listFragment.addDeleteFavorite(businessId, r.isVisited());
+                    r.setVisited(!r.isVisited());
+                    setImageView(finalHolder.visited, r.isVisited());
+                }
+            });
+
+            holder.ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                @Override
+                public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                    //Do nothing;
+                }
+            });
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Prepare all the data we need to start map activity.
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable(
+                            RestaurantMapActivity.EXTRA_LATLNG,
+                            new LatLng(r.getLat(), r.getLng()));
+                    Intent intent = new Intent(view.getContext(), RestaurantMapActivity.class);
+                    intent.putExtras(bundle);
+                    context.startActivity(intent);
+                }
+            });
+            convertView.setTag(holder);
+        } else {
+            holder = (Holder) convertView.getTag();
+        }
+
+
+        holder.restaurantName.setText(r.getName());
+        holder.restaurantAddress.setText(r.getAddress());
+        holder.restaurantType.setText(r.getCategories().get(0));
+        holder.restaurantThumbnail.setImageBitmap(r.getThumbnail());
+        holder.ratingBar.setRating((float) r.getStars());
+        setImageView(holder.visited, r.isVisited());
+        return convertView;
+    }
+
 }
